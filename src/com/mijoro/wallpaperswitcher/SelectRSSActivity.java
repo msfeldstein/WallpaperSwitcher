50e70b6acb174244ff1092abc6a30b62fcf6abdd
Michael Feldstein <msfeldstein@gmail.com>
2013-02-24 15:24:31 -0600

    Always show the launcher after setting the wallpaper

--- src/com/mijoro/wallpaperswitcher/SelectRSSActivity.java
+++ src/com/mijoro/wallpaperswitcher/SelectRSSActivity.java
@@ -104,6 +104,10 @@ public class SelectRSSActivity extends Activity implements LatestImageFetcher.Im
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		Intent startMain = new Intent(Intent.ACTION_MAIN);
+        startMain.addCategory(Intent.CATEGORY_HOME);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(startMain);
 		finish();
 	}
 	
